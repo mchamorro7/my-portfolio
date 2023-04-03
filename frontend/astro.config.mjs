2f79907e1c2f53d8d98791658f1de56587d20d6e
Mateo Chamorro <mateochamorro@hotmail.com>
2023-04-03 00:42:16 -0300

    chore: add latest sanity api version

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,23 +1,24 @@
-import { defineConfig } from "astro/config";
-import sanity from "astro-sanity";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import sanity from 'astro-sanity'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: "xqwb2285",
-      dataset: "production",
-      useCdn: true,
+      projectId: 'xqwb2285',
+      dataset: 'production',
+      apiVersion: '2023-03-28',
+      useCdn: true
     }),
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
       config: {
-        applyBaseStyles: false,
-      },
+        applyBaseStyles: false
+      }
     }),
-    react(),
-  ],
-});
+    react()
+  ]
+})
