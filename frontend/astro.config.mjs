f444b30365228c0adc09548ef3b1108c03e24a77
Mateo Chamorro <mateochamorro@hotmail.com>
2023-04-03 00:05:08 -0300

    chore: tailwind and react integration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,23 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import sanity from "astro-sanity";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: 'xqwb2285',
-      dataset: 'production',
+      projectId: "xqwb2285",
+      dataset: "production",
       useCdn: true,
-    })
-  ]
+    }),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+  ],
 });
