332bda161b74ded23c3e1a36390b1485268a0770
Mateo Chamorro <mateochamorro@hotmail.com>
2023-04-07 23:21:10 -0300

    fix: tailwind theme colors

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,15 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    colors: {
-      primary: '#00FFF0',
-      secondary: '#181A20'
-    },
     fontFamily: {
       title: ['Montserrat'],
       body: ['Lato']
     },
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#00FFF0',
+        secondary: '#181A20'
+      }
+    }
   },
   plugins: []
 }
