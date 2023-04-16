58dff82d4fb249db89ce4e4609130204bf7168ed
Mateo Chamorro <mateochamorro@hotmail.com>
2023-04-16 15:07:42 -0300

    feat: add job and project schemas

---
+++ studio/schemas/project.ts
@@ -0,0 +1,69 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      validation: Rule => Rule.required().min(10).max(100)
+    }),
+    defineField({
+      name: 'date',
+      title: 'Date',
+      type: 'date',
+      options: {
+        dateFormat: 'YYYY-MM'
+      },
+      validation: Rule => Rule.required()
+    }),
+    defineField({
+      name: 'stack',
+      title: 'Tech stack',
+      type: 'array',
+      of: [
+        {
+          title: 'Technology',
+          type: 'string',
+          options: {
+            layout: 'tags'
+          }
+        },
+      ],
+      validation: Rule => Rule.required()
+    }),
+    defineField({
+        name: 'code',
+        title: 'GitHub repository',
+        type: 'string',
+    }),
+    defineField({
+        name: 'demo',
+        title: 'Demo',
+        type: 'string',
+    }),
+    defineField({
+        name: 'thumbnail',
+        title: 'Thumbnail',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      media: 'thumbnail',
+    },
+  },
+})
