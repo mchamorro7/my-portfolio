76592446de1a3611b4e98a9bd4c2a933d225f57a
Mateo Chamorro <mateochamorro@hotmail.com>
2023-04-16 16:06:47 -0300

    feat: sanity graphql configuration

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,8 +1,12 @@
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
-  api: {
-    projectId: 'xqwb2285',
-    dataset: 'production'
-  }
+  graphql: [
+    {
+      playground: true,
+      tag: 'default',
+      workspace: 'default',
+      id: 'schema-production',
+    },
+  ]
 })
